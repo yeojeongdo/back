c755f490304d6e5d3929604bbf47fc61b0ac6bfd
조민수 <chominsu10@gmail.com>
2021-09-13 18:01:24 +0900

    [ADD] mobx Provider

---
+++ src/Stores/index.ts
@@ -0,0 +1,8 @@
+// import AuthStore from "./Auth"
+
+const stores = {
+  //이렇게 선언 하면 됨
+  // AuthStore: new AuthStore(),
+};
+
+export default stores;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "mobx-react";
+import stores from "./Stores";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={stores}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
