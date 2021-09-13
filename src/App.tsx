9bcec03cb64296f2be807057251d55ed5bf7608a
HongJunHyeok <edb1631@naver.com>
2021-09-13 20:21:55 +0900

    TEST: Router State maintain

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Counter from "components/Counter";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
       여정도
       <Counter />
+      <Link to="/other">다른페이지</Link>
     </div>
   );
 }
---
+++ src/pages/Other.tsx
@@ -0,0 +1,10 @@
+import { observer } from "mobx-react";
+
+const Other = () => {
+  return (
+    <div>
+      <div>Other</div>
+    </div>
+  );
+};
+export default observer(Other);
