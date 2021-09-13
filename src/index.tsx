4d9444d182a37276035e82848aed7f5269a4b616
HongJunHyeok <edb1631@naver.com>
2021-09-13 20:11:12 +0900

    ADD: Counter Mobx example

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Provider } from "mobx-react";
-import stores from "./stores";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={stores}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,7 @@
 import { observer } from "mobx-react";
 import React from "react";
-import useStores from "../hooks/useStore";
 
 const Main = () => {
-  const { store } = useStores();
-  console.log(store);
   return (
     <div>
       <div>Main</div>
