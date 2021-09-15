aac0b290915013ed7535acb04cd410861141033c
HongJunHyeok <edb1631@naver.com>
2021-09-16 08:54:45 +0900

    EDIT: Login Form Style

--- src/components/Auth/LoginForm/LoginForm.tsx
+++ src/components/Auth/LoginForm/LoginForm.tsx
@@ -1,13 +1,16 @@
 import Form from "components/Common/Form/Form";
 import Input from "components/Common/Input/Input";
+import { LoginContainer } from "./loginFromStyles";
 
 const LoginForm: React.VFC = () => {
   return (
     <Form hasSubmit submitText="로그인">
-      <h1>로그인</h1>
-      <p>여정도에서 사람들의 여정을 찾아보세요.</p>
-      <Input type="text" placeholder="아이디를 입력해주세요." />
-      <Input type="password" placeholder="비밀번호를 입력해주세요." />
+      <LoginContainer>
+        <h1>로그인</h1>
+        <p>여정도에서 사람들의 여정을 찾아보세요.</p>
+        <Input type="text" placeholder="아이디를 입력해주세요." />
+        <Input type="password" placeholder="비밀번호를 입력해주세요." />
+      </LoginContainer>
     </Form>
   );
 };
--- src/components/Auth/LoginForm/loginFromStyles.tsx
+++ src/components/Auth/LoginForm/loginFromStyles.tsx
@@ -1,3 +1,11 @@
 import styled from "@emotion/styled";
 
-export const LoginContainer = styled.div``;
+export const LoginContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  & > input {
+    width: 100%;
+  }
+`;
