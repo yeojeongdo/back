8b8fc6353447267b6e9593aec055dc8fc61fa8cd
HongJunHyeok <edb1631@naver.com>
2021-09-23 14:25:41 +0900

    EDIT: Routing Component

---
+++ src/Routes.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import LoginPage from "pages/LoginPage/LoginPage";
+import JoinPage from "pages/JoinPage/JoinPage";
+import Main from "pages/Main";
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/join">
+          <JoinPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import LoginPage from "pages/LoginPage/LoginPage";
-import JoinPage from "pages/JoinPage/JoinPage";
+import Routes from "Routes";
+import GlobalStyle from "styles/globalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <App />
-        </Route>
-        <Route exact path="/login">
-          <LoginPage />
-        </Route>
-        <Route exact path="/join">
-          <JoinPage />
-        </Route>
-      </Switch>
-    </Router>
+    <GlobalStyle />
+    <Routes />
   </React.StrictMode>,
   document.getElementById("root")
 );
