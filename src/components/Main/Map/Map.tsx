e35ac3b42b7b92bcab0a376eb5c0bd1423a7b55c
HongJunHyeok <edb1631@naver.com>
2021-09-26 01:25:30 +0900

    ADD: Get Current Position

--- src/components/Main/Map/Map.tsx
+++ src/components/Main/Map/Map.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { StyledMap } from "./mapStyles";
 
 declare global {
@@ -8,16 +8,33 @@ declare global {
 }
 
 const Map = () => {
+  const [latitude, setLatitude] = useState<number>(0);
+  const [longTitude, setLongTitude] = useState<number>(0);
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      /* 위치정보 사용 가능 */
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLatitude(position.coords.latitude);
+        setLongTitude(position.coords.longitude);
+      });
+    } else {
+      /* 위치정보 사용 불가능 */
+      alert(
+        "죄송합니다. 저희 서비스를 이용하실 수 없습니다. (브라우저 버전을 업그레이드 해보세요)"
+      );
+    }
+  }, []);
   useEffect(() => {
     let container = document.getElementById("map");
     let options = {
-      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      center: new window.kakao.maps.LatLng(latitude, longTitude),
       level: 3,
     };
 
     let map = new window.kakao.maps.Map(container, options);
     console.log(map);
-  }, []);
+  }, [latitude, longTitude]);
 
   return (
     <>
