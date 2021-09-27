7c5bcf0f5ce360c2f6534731087616c5be78700a
HongJunHyeok <edb1631@naver.com>
2021-09-27 12:04:14 +0900

    ADD: Logout mutation

--- src/components/Common/Header/headerStyles.tsx
+++ src/components/Common/Header/headerStyles.tsx
@@ -2,9 +2,11 @@ import styled from "@emotion/styled";
 import { MAIN_COLOR } from "styles/colors";
 
 export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   width: 100%;
   padding: 10px 20px;
-  line-height: 1rem;
   background-color: ${MAIN_COLOR};
   position: fixed;
   left: 0;
@@ -12,5 +14,11 @@ export const HeaderContainer = styled.header`
   z-index: 9999;
   .header-title {
     font-size: 16px;
+  }
+  .header-options {
+    list-style: none;
+    & > .logout {
+      cursor: pointer;
+    }
   }
 `;
