56480104c74b8e0942b13a36c4e792637e34eb48
조민수 <chominsu10@gmail.com>
2021-09-28 09:13:47 +0900

    [ADD] joinForm

--- src/components/Auth/JoinForm/joinFormStyles.tsx
+++ src/components/Auth/JoinForm/joinFormStyles.tsx
@@ -21,4 +21,26 @@ export const JoinFormContainer = styled.div`
     display: flex;
     flex-direction: column;
   }
+  .flexBox {
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  .flexBox > div {
+    width: 50%;
+  }
+
+  .name > input {
+    width: 97.5%;
+  }
+  .sexButtons {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+  }
+  .sexButtons > button {
+    width: 45%;
+    height: 50px;
+  }
 `;
