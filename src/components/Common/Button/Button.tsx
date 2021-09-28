b0e1327062059fe7cb880c4c22e5c630b2c0a35d
HongJunHyeok <edb1631@naver.com>
2021-09-28 12:25:24 +0900

    EDIT: Button Props

--- src/components/Common/Button/Button.tsx
+++ src/components/Common/Button/Button.tsx
@@ -1,14 +1,26 @@
 import { ButtonContainer } from "./buttonStyles";
 
 interface ButtonProps {
-  onClick?: (e?: any) => void;
-  type?: "button" | "submit" | undefined;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset" | undefined;
+  className?: string;
   style?: any;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick, style, type }) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  style,
+  type,
+  className,
+}) => {
   return (
-    <ButtonContainer style={style} onClick={onClick} type={type}>
+    <ButtonContainer
+      style={style}
+      onClick={onClick}
+      type={type}
+      className={className}
+    >
       {children}
     </ButtonContainer>
   );
