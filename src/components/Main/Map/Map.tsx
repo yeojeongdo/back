4ec225c662f132c9e187be8219641fd36a6960f1
조민수 <chominsu10@gmail.com>
2021-09-29 14:49:52 +0900

    [ADD] map Clusterer click => 대상 마커만 활성화

--- src/components/Main/Map/Map.tsx
+++ src/components/Main/Map/Map.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import { StyledMap } from "./mapStyles";
-import { Map as CustomMap, MapMarker } from "react-kakao-maps-sdk";
+import {
+  Map as CustomMap,
+  MapMarker,
+  MarkerClusterer,
+} from "react-kakao-maps-sdk";
 
-declare global {
-  interface Window {
-    kakao: any;
-  }
+interface mapType {
+  albums: Album[];
+  setAlbums: React.Dispatch<React.SetStateAction<Album[]>>;
 }
 
-const Map: React.VFC<{ albums: Album[] }> = ({ albums = [] }) => {
+const Map = ({ albums, setAlbums }: mapType) => {
   const [latitude, setLatitude] = useState<number>(35.6632143);
   const [longTitude, setLongTitude] = useState<number>(128.4140176);
   return (
@@ -20,29 +23,48 @@ const Map: React.VFC<{ albums: Album[] }> = ({ albums = [] }) => {
       }}
       style={{
         // 지도의 크기
-        width: "684px",
+        flex: 3,
         height: "100%",
       }}
       level={3}
     >
-      {albums.map((album, index) => (
-        <MapMarker
-          position={{
-            lat:
-              album.latitude +
-              index /
-                100 /* 더미데이터의 값이 같기떄문에 차이가 보이지 않아 임시로 지정 이후 삭제바람 */,
-            lng: album.longitude,
-          }}
-          image={{
-            src: album.photo,
-            size: {
-              width: 24,
-              height: 35,
-            },
-          }}
-        />
-      ))}
+      <MarkerClusterer
+        averageCenter={true} // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+        minLevel={5} // 클러스터 할 최소 지도 레벨
+        disableClickZoom={true} // 클러스터 마커를 클릭했을 때 지도가 확대되지 않도록 설정한다
+        onClusterclick={(target, cluster) => {
+          const markers: Album[] = [];
+          cluster.getMarkers().map(marker => {
+            albums.map(album => {
+              if (album.address === marker.getTitle()) {
+                markers.push(album);
+              }
+              return 0;
+            });
+          });
+          setAlbums(markers);
+        }}
+      >
+        {albums.map((album, index) => (
+          <MapMarker
+            position={{
+              lat:
+                album.latitude +
+                index /
+                  100 /* 더미데이터의 값이 같기떄문에 차이가 보이지 않아 임시로 지정 이후 삭제바람 */,
+              lng: album.longitude,
+            }}
+            image={{
+              src: album.photo,
+              size: {
+                width: 24,
+                height: 35,
+              },
+            }}
+            title={album.address}
+          />
+        ))}
+      </MarkerClusterer>
     </CustomMap>
   );
 };
