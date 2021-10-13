57df52fb5f88e6e3f09e15229b8c50fbd9643f5f
HongJunHyeok <edb1631@naver.com>
2021-10-13 20:19:02 +0900

    FIX: Edit comment rendering error

--- src/components/Main/Album/AlbumComment/AlbumComment.tsx
+++ src/components/Main/Album/AlbumComment/AlbumComment.tsx
@@ -6,10 +6,11 @@ import {
   CommentListContainer,
 } from "./albumCommentStyles";
 import DefaultProfile from "assets/images/default_profile.svg";
-import { useCallback, useEffect, VFC } from "react";
+import { createRef, useCallback, useEffect, useState, VFC } from "react";
 import ReactLoading from "react-loading";
 import useInput from "hooks/useInput";
 import useAuth from "hooks/redux/useAuth";
+import { toast } from "react-toastify";
 
 interface IAlbumCommentProps {
   commentInputRef: any;
@@ -20,7 +21,20 @@ const AlbumComment: VFC<IAlbumCommentProps> = ({ commentInputRef }) => {
     useAlbum();
   const { authState } = useAuth();
 
+  const editCommentRef = createRef<HTMLTextAreaElement>();
+
   const [commentInput, onChangeCommentInput, setCommentInput] = useInput("");
+  const [isToggleMode, setIsToggleMode] = useState<boolean>(false);
+  const [editCommentText, onChangeEditCommentText] = useInput("");
+  const toggleEditMode = useCallback((mode: "OPEN" | "CLOSE") => {
+    if (mode === "OPEN") {
+      setIsToggleMode(true);
+    } else if (mode === "CLOSE") {
+      setIsToggleMode(false);
+    } else {
+      throw new Error(`유효하지 않은 전달값입니다.`);
+    }
+  }, []);
 
   const handleCreateComment = useCallback(
     (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
@@ -49,18 +63,11 @@ const AlbumComment: VFC<IAlbumCommentProps> = ({ commentInputRef }) => {
 
   const handleEditComment = useCallback(
     (commentId: number) => {
-      const data = { id: commentId, comment: "" };
+      const data = { id: commentId, comment: editCommentText };
       editComment(data);
     },
-    [editComment]
+    [editCommentText, editComment]
   );
-
-  useEffect(() => {
-    //* 댓글 삭제 후 댓글리스트 렌더링
-    if (albumState.deleteCommentDone && albumState.album?.id) {
-      getComments(albumState.album.id);
-    }
-  }, [albumState.deleteCommentDone, albumState.album?.id, getComments]);
 
   useEffect(() => {
     //* 댓글 생성 후 댓글리스트 렌더링
@@ -76,10 +83,37 @@ const AlbumComment: VFC<IAlbumCommentProps> = ({ commentInputRef }) => {
   ]);
 
   useEffect(() => {
+    //* 댓글 삭제 후 댓글리스트 렌더링
+    if (albumState.deleteCommentDone && albumState.album?.id) {
+      toast.success("삭제 완료");
+      getComments(albumState.album.id);
+    }
+  }, [albumState.deleteCommentDone, albumState.album?.id, getComments]);
+
+  useEffect(() => {
+    //* 댓글 수정 후 수정모드 토글
+    if (albumState.editCommentDone && albumState.album?.id) {
+      toggleEditMode("CLOSE");
+      getComments(albumState.album.id);
+    }
+  }, [
+    toggleEditMode,
+    albumState.album?.id,
+    albumState.editCommentDone,
+    getComments,
+  ]);
+
+  useEffect(() => {
     if (commentInputRef.current) {
       autosize(commentInputRef.current);
     }
   }, [commentInputRef]);
+
+  useEffect(() => {
+    if (editCommentRef.current) {
+      autosize(editCommentRef.current);
+    }
+  }, [editCommentRef]);
 
   useEffect(() => {
     if (albumState.album?.id) {
@@ -96,15 +130,46 @@ const AlbumComment: VFC<IAlbumCommentProps> = ({ commentInputRef }) => {
           {albumState.comments?.map((comment) => (
             <CommentItemContainer key={comment.id}>
               <span className="comment_userName">{comment.user.name}</span>
-              <p className="comment_content">{comment.content}</p>
-              {authState.myInfo?.id === comment.user.id && (
+              {isToggleMode && authState.myInfo?.id === comment.user.id ? (
                 <>
-                  <button onClick={() => handleEditComment(comment.id)}>
-                    수정
-                  </button>
-                  <button onClick={() => handleDeleteComment(comment.id)}>
-                    삭제
-                  </button>
+                  <textarea
+                    ref={editCommentRef}
+                    className="edit_textarea"
+                    placeholder="수정할 텍스트를 입력하세요."
+                    value={editCommentText}
+                    onChange={onChangeEditCommentText}
+                  />
+                  <div className="edit_buttons">
+                    <button
+                      className="edit_buttons_edit"
+                      onClick={() => handleEditComment(comment.id)}
+                    >
+                      수정하기
+                    </button>
+                    <button
+                      onClick={() => toggleEditMode("CLOSE")}
+                      className="edit_buttons_cancel"
+                    >
+                      취소
+                    </button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <p className="comment_content">{comment.content}</p>
+                  {authState.myInfo?.id === comment.user.id && (
+                    <div className="edit_buttons">
+                      <button
+                        onClick={() => toggleEditMode("OPEN")}
+                        className="edit_buttons_edit"
+                      >
+                        수정
+                      </button>
+                      <button onClick={() => handleDeleteComment(comment.id)}>
+                        삭제
+                      </button>
+                    </div>
+                  )}
                 </>
               )}
             </CommentItemContainer>
