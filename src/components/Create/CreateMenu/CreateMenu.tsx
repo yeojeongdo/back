d6cea4c582791a285a560eaa8f1d75055471057d
조민수 <chominsu10@gmail.com>
2021-10-17 22:13:28 +0900

    [ADD] mapSearch Test

--- src/components/Create/CreateMenu/CreateMenu.tsx
+++ src/components/Create/CreateMenu/CreateMenu.tsx
@@ -14,7 +14,7 @@ const CreateMenu = () => {
 
   const submit = useCallback(() => {
     const form = new FormData();
-    form.append("address", "대구");
+    form.append("address", "대구 어디");
     form.append("latitude", latLng.lat.toString());
     form.append("longitude", latLng.lng.toString());
     form.append("memo", memo);
@@ -43,7 +43,7 @@ const CreateMenu = () => {
 
   return (
     <CreateMenuContainer>
-      {latLng.lat / latLng.lng}
+      {latLng.lat + "/" + latLng.lng}
       <Form hasSubmit submitText="작성하기" onSubmit={submit}>
         {preview && <img src={preview.toString()} alt="" />}
         <Input type="file" id="albumFile" multiple onChange={handleFileInput} />
--- src/components/Create/CreaterMap/CreaterMap.tsx
+++ src/components/Create/CreaterMap/CreaterMap.tsx
@@ -1,20 +1,41 @@
+import Input from "components/Common/Input/Input";
 import useCreate from "hooks/redux/useCreate";
+import useInput from "hooks/useInput";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 const CreaterMap = () => {
   const { setMarker, markerState } = useCreate();
+  const [text, onChangeText] = useInput("대구 소프트웨어 마이스터 고등학교");
+  const [map, setMap] = useState<kakao.maps.Map>();
+  useEffect(() => {
+    if (!map) return;
+    const ps = new kakao.maps.services.Places();
+
+    ps.keywordSearch(text, (data, status) => {
+      // const bounds = new kakao.maps.LatLngBounds()
+      if (status === kakao.maps.services.Status.OK) {
+        data && console.log(data);
+        data.map(({ x, y }) => {
+          setMarker({ lat: y, lng: x });
+        });
+      }
+    });
+  }, [text, onChangeText, setMarker]);
+
   return (
     <Map
-      center={{
+      center={
         // 지도의 중심좌표
-        lat: 33.450701,
-        lng: 126.570667,
-      }}
+        markerState.LatLng
+      }
       style={{
         // 지도의 크기
         flex: "3 1 0%",
         height: "100%",
       }}
+      onCreate={setMap}
       level={3}
       onClick={(_t, mouseEvent: any) => {
         console.log(mouseEvent);
@@ -35,7 +56,9 @@ const CreaterMap = () => {
             });
           }}
           draggable={true}
-        />
+        >
+          <Input value={text} onChange={onChangeText} />
+        </MapMarker>
       )}
     </Map>
   );
